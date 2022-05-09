a5b0b180f17d23c511c2cd6b6e883d999b8b8bf3
codebr3ak <barakaralph@gmail.com>
2022-05-09 14:44:35 +0300

    /..

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,20 @@
           }
         }
       );
+
+// new menu click
+        var menuList = document.getElementById('menuList');
+
+        menuList.style.maxHeight = "0px";
+
+        function togglemenu(){
+
+          if(menuList.style.maxHeight == "0px")
+          {
+            menuList.style.maxHeight = "130px"
+          }
+          else
+          {
+            menuList.style.maxHeight = "0px"
+          }
+        }
